b75ccc5c23834847dd26c9ce5c9ceb4030ba9532
sassy <art52wind@gmail.com>
2025-06-11 22:08:34 +0900

    Add missing STTP http client backend

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,7 @@ object Dependencies {
   object Libraries {
     val sttp = "com.softwaremill.sttp.client3" %% "core" % "3.8.6"
     val httpCirce = "com.softwaremill.sttp.client3" %% "circe" % "3.8.6"
+    val httpClientBackend = "com.softwaremill.sttp.client3" %% "httpclient-backend" % "3.8.6"
     val circe = Seq(
       "io.circe" %% "circe-core" % circeVersion,
       "io.circe" %% "circe-generic" % circeVersion,
